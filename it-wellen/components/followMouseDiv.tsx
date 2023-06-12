2aa6ac953c83a429d3b35d45c05094ce2790c4b5
Engjëll <57192274+engjellhas@users.noreply.github.com>
2023-06-12 16:26:51 +0200

    Update followMouseDiv.tsx

--- it-wellen/components/followMouseDiv.tsx
+++ it-wellen/components/followMouseDiv.tsx
@@ -21,14 +21,14 @@ const FollowMouseDiv = () => {
     <div
       style={{
         position: "fixed",
-        width: "100px",
-        height: "100px",
+        width: "120px",
+        height: "120px",
         left: `${mousePosition.x - 50}px`,
         top: `${mousePosition.y - 50}px`,
         background: "#14308B",
         opacity: "0.7",
-        filter: "blur(80px)",
-        borderRadius: "20px",
+        filter: "blur(40px)",
+        borderRadius: "50px",
         zIndex: "-10",
       }}
     ></div>
